8f80b3ba205b22e0d6c80fa93a09eec7fcfde7ba
Darcy Laycock <sutto@sutto.net>
2010-05-03 01:07:03 +0800

    Misc fixes

--- lib/shuriken.rb
+++ lib/shuriken.rb
@@ -1,6 +1,6 @@
 module Shuriken
   
-  VERSION = "0.1.0".freeze
+  VERSION = "0.1.1".freeze
   
   def self.register_framework!
     Barista::Framework.register 'shuriken', File.expand_path('../coffeescripts', File.dirname(__FILE__))
