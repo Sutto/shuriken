f95063da597428133e7df001e4e44a7048b14dcf
Darcy Laycock <sutto@sutto.net>
2010-08-21 17:07:48 +0800

    Bump version, CoffeeScript 0.9.1 compatible

--- lib/shuriken.rb
+++ lib/shuriken.rb
@@ -1,6 +1,6 @@
 module Shuriken
   
-  VERSION = "0.1.4".freeze
+  VERSION = "0.2.0".freeze
   
   def self.register_framework!
     Barista::Framework.register 'shuriken', File.expand_path('../coffeescripts', File.dirname(__FILE__))
